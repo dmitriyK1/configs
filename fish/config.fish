dd19e149517cba9840529c0c7df00d124e799573
Dmitriy Kaminskiy <dimaroxx@gmail.com>
2017-02-26 14:42:47 +0200

    update fish config

---
+++ fish/config.fish
@@ -0,0 +1,33 @@
+[ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
+
+# fish git prompt
+# set __fish_git_prompt_showdirtystate 'yes'
+# set __fish_git_prompt_showstashstate 'yes'
+# set __fish_git_prompt_showupstream 'yes'
+# set __fish_git_prompt_color_branch yellow
+
+# Status Chars
+# set __fish_git_prompt_char_dirtystate '⚡'
+# set __fish_git_prompt_char_stagedstate '→'
+# set __fish_git_prompt_char_stashstate '↩'
+# set __fish_git_prompt_char_upstream_ahead '↑'
+# set __fish_git_prompt_char_upstream_behind '↓'
+#
+# function fish_prompt
+#   set last_status $status
+#   set_color $fish_color_cwd
+#   printf '%s' (prompt_pwd)
+#   set_color normal
+#   printf '%s ' (__fish_git_prompt)
+#   set_color normal
+# end
+
+# test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+# source ~/.iterm2_shell_integration.fish
+
+function fish_prompt
+  ~/Soft/powerline-shell/powerline-shell.py $status --shell bare ^/dev/null
+end
+
+source ~/.fish_aliases
+source ~/.fish_variables

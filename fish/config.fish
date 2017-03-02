40fc8657c06cd5e4b7e8acdb08f134b7f2c60c2c
Dmitriy Kaminskiy <dimitriy.kaminskiy@nextiva.com>
2017-03-02 16:24:16 +0200

    update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -29,22 +29,6 @@ function fish_prompt
   ~/Soft/powerline-shell/powerline-shell.py $status --shell bare ^/dev/null
 end
 
-# To just rerun your last command, simply type '!!'
-# '!! sudo' will prepend sudo to your most recent command
-# Running !! with anything other than sudo will append the argument to your most recent command
-# To add another command to prepend list remove the # on line 10 and put the command in the quotes. Repeat as needed
-function !!;
-  set var (history | head -n 1)
-  if test $argv
-    if test $argv = "sudo"        #; or "any other command you want to prepend"
-        eval $argv $var
-    else
-        eval $var $argv
-    end
-    else
-        eval $var
-  end
-end
-
 source ~/.fish_aliases
 source ~/.fish_variables
+source ~/.fish_functions

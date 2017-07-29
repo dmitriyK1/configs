8a7dee008eeeae8799bb91646c97688274f81bff
Dmitriy Kaminskiy <dimaroxx@gmail.com>
2017-07-29 21:49:23 +0300

    update hyperterm config

--- .hyper.js
+++ .hyper.js
@@ -5,7 +5,7 @@ module.exports = {
     fontSize: 12,
 
     // font family with optional fallbacks
-    fontFamily: 'Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+    fontFamily: '"DejaVuSansMono Nerd Font", Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
 
     // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
     cursorColor: 'rgba(248,28,229,0.8)',

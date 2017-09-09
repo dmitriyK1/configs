cdb6735141ae9bef239544e301786acfe8bd22ba
Dmitriy Kaminskiy <dimaroxx@gmail.com>
2017-09-10 01:14:29 +0300

    update emacs config

--- .emacs.d/config/init-package.el
+++ .emacs.d/config/init-package.el
@@ -20,8 +20,7 @@
 
 ; list the repositories containing them
 (setq package-archives
-      '(("elpa"        . "http://tromey.com/elpa/")
-        ("gnu"         . "http://elpa.gnu.org/packages/")
+      '(("gnu"         . "http://elpa.gnu.org/packages/")
         ("org"         . "http://orgmode.org/elpa/")
         ("melpa"       . "http://melpa.org/packages/")
         ("marmalade"   . "http://marmalade-repo.org/packages/")))

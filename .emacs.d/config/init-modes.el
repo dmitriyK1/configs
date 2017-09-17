0a4b7a0bb51e2a237aec109ac3041b3746c531fe
Dmitriy Kaminskiy <dimaroxx@gmail.com>
2017-09-17 14:25:27 +0300

    update emacs config

--- .emacs.d/config/init-modes.el
+++ .emacs.d/config/init-modes.el
@@ -12,4 +12,7 @@
 ;; colorize parentheses in different colors
 (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
 
+;; save state of a previous session (buffers, modes)
+(desktop-save-mode 1)
+
 (provide 'init-modes)

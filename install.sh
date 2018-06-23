4a68212b4b50b71bc4f364c31ccf4fa16cdd8994
Dmitriy Kaminskiy <dimitriy.kaminskiy@nextiva.com>
2018-06-23 22:00:41 +0300

    add install script

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env fish
+
+echo "Installing npm packages"
+sudo npm i -g npm-check-updates@latest
+sudo npm i -g diff-so-fancy@latest
+sudo npm i -g nodemon@latest
+sudo npm i -g serve@latest
+sudo npm i -g git-recent@latest
+sudo npm i -g create-react-app@latest
+sudo npm i -g tldr@latest --unsafe-perm=true
+sudo npm i -g ntl@latest
+sudo npm i -g caniuse@latest

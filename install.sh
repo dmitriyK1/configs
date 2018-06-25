5dc669359f4922a2c07d86609e2e91622ed2a6c9
Dmitriy Kaminskiy <dimitriy.kaminskiy@nextiva.com>
2018-06-26 02:51:06 +0300

    update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ sudo npm i -g git-recent@latest
 sudo npm i -g create-react-app@latest
 sudo npm i -g tldr@latest --unsafe-perm=true
 sudo npm i -g ntl@latest
-sudo npm i -g caniuse-cmd@latest
+sudo npm i -g caniuse-cli@latest
 sudo npm i -g fkill-cli@latest
 sudo npm i -g cz-conventional-changelog@latest
 sudo npm i -g npm-merge-driver@latest

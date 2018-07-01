8fa160fa5479f171614b3d55261f9545ed676b53
Dmitriy Kaminskiy <dimitriy.kaminskiy@nextiva.com>
2018-07-01 07:27:34 +0300

    update emacs config

--- emacs-minimal-config-with-packages/init.el
+++ emacs-minimal-config-with-packages/init.el
@@ -1,5 +1,34 @@
 ;; https://gist.github.com/huytd/6b785bdaeb595401d69adc7797e5c22c
 
+;; Always load newest byte code
+(setq load-prefer-newer t)
+
+;; reduce the frequency of garbage collection by making it happen on
+;; each 50MB of allocated data (the default is on every 0.76MB)
+(setq gc-cons-threshold 50000000)
+
+;; nice scrolling
+(setq scroll-margin 0
+      scroll-conservatively 100000
+      scroll-preserve-screen-position 1)
+
+;; mode line settings
+(line-number-mode t)
+(column-number-mode t)
+(size-indication-mode t)
+
+;; Newline at end of file
+(setq require-final-newline t)
+
+;; store all backup and autosave files in the tmp dir
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
+
+;; revert buffers automatically when underlying files are changed externally
+(global-auto-revert-mode t)
+
 (set-frame-parameter nil 'fullscreen 'fullscreen)
 
 (setq inhibit-startup-screen t)
@@ -95,8 +124,12 @@
   "w/"  '(split-window-right :which-key "split right")
   "w-"  '(split-window-below :which-key "split bottom")
   "wx"  '(delete-window :which-key "delete window")
+  ;; Git
+  "G"   '(magit :which-key "magit")
+  "gs"  '(magit-status :which-key "git status")
   ;; Others
   "at"  '(ansi-term :which-key "open terminal")
+  "q"   '(save-buffers-kill-emacs :which-key "quit emacs")
 ))
 
 (use-package magit
@@ -105,10 +138,38 @@
 (use-package evil-magit
   :ensure t)
 
-(use-package evil-commentary
+;; Emacs key bindings
+;; (global-set-key (kbd "M-;") 'evilnc-comment-or-uncomment-lines)
+;; (global-set-key (kbd "C-c l") 'evilnc-quick-comment-or-uncomment-to-the-line)
+;; (global-set-key (kbd "C-c c") 'evilnc-copy-and-comment-lines)
+;; (global-set-key (kbd "C-c p") 'evilnc-comment-or-uncomment-paragraphs)
+(use-package evil-nerd-commenter
   :ensure t
-  :config
-  (evil-commentary-mode))
+  :config (evilnc-default-hotkeys)
+)
+
+;; Vim key bindings
+(use-package evil-leader
+  :ensure t)
+(global-evil-leader-mode)
+(evil-leader/set-leader ",")
+(evil-leader/set-key
+  "ci" 'evilnc-comment-or-uncomment-lines
+  "cl" 'evilnc-quick-comment-or-uncomment-to-the-line
+  "ll" 'evilnc-quick-comment-or-uncomment-to-the-line
+  "cc" 'evilnc-copy-and-comment-lines
+  "cp" 'evilnc-comment-or-uncomment-paragraphs
+  "cr" 'comment-or-uncomment-region
+  "cv" 'evilnc-toggle-invert-comment-line-by-line
+  "."  'evilnc-copy-and-comment-operator
+  "\\" 'evilnc-comment-operator ; if you prefer backslash key
+  ","  'save-buffer
+)
+
+;; (use-package evil-commentary
+;;   :ensure t
+;;   :config
+;;   (evil-commentary-mode))
 
 ;; no bell
 (setq ring-bell-function 'ignore)
@@ -137,10 +198,30 @@
 
 ;; UTF-8 please
 (setq locale-coding-system 'utf-8) ; pretty
+(set-default-coding-systems 'utf-8)
 (set-terminal-coding-system 'utf-8) ; pretty
 (set-keyboard-coding-system 'utf-8) ; pretty
 (set-selection-coding-system 'utf-8) ; please
 (prefer-coding-system 'utf-8) ; with sugar on top
+
+;; hippie expand is dabbrev expand on steroids
+(setq hippie-expand-try-functions-list '(try-expand-dabbrev
+                                         try-expand-dabbrev-all-buffers
+                                         try-expand-dabbrev-from-kill
+                                         try-complete-file-name-partially
+                                         try-complete-file-name
+                                         try-expand-all-abbrevs
+                                         try-expand-list
+                                         try-expand-line
+                                         try-complete-lisp-symbol-partially
+                                         try-complete-lisp-symbol))
+
+;; use hippie-expand instead of dabbrev
+(global-set-key (kbd "M-/") #'hippie-expand)
+(global-set-key (kbd "s-/") #'hippie-expand)
+
+;; replace buffer-menu with ibuffer
+(global-set-key (kbd "C-x C-b") #'ibuffer)
 
 ;; Show keystrokes in progress
 (setq echo-keystrokes 0.1)
@@ -167,20 +248,38 @@
 ;; for ripgrep to work
 (setq exec-path
   '(
-   "/usr/local/bin"
+    "/bin/"
+    "/usr/local/bin"
   )
 )
 
 ;; save on focus lost (without trashing a minibuffer)
 (add-hook 'focus-out-hook (lambda () (cl-flet ((message (format &rest args) nil)) (save-some-buffers t))))
 
-(recentf-mode 1)
-(setq recentf-max-menu-items 25)
-;; (global-set-key "\C-x\ \C-r" 'recentf-open-files)
 (global-set-key "\C-x\ \C-r" 'helm-recentf)
-;; run periodically
-(run-at-time nil (* 5 60) 'recentf-save-list)
 
 ;; Save place in files between Sessions
 (save-place-mode 1)
 (setq save-place-forget-unreadable-files nil)
+
+(electric-pair-mode 1)
+;; make electric-pair-mode work on more brackets
+(setq electric-pair-pairs '(
+                            (?\" . ?\")
+                            (?\{ . ?\})
+                            ) )
+
+;; dired - reuse current buffer by pressing 'a'
+(put 'dired-find-alternate-file 'disabled nil)
+
+(use-package anzu
+  :ensure t
+  :bind (("M-%" . anzu-query-replace)
+         ("C-M-%" . anzu-query-replace-regexp))
+  :config
+  (global-anzu-mode))
+
+(use-package ace-window
+  :ensure t
+  :config
+  (global-set-key (kbd "M-o") 'ace-window))

58640c384f7d67ebf205420b4b0ebf69f2e9a361
Dmitriy Kaminskiy <dimaroxx@gmail.com>
2018-11-26 23:11:59 +0200

    update osx.sh

--- osx.sh
+++ osx.sh
@@ -140,6 +140,8 @@ defaults write NSGlobalDomain InitialKeyRepeat -int 10
 echo "Disabling auto-correct..."
 defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
 
+# Stop iTunes from responding to the keyboard media keys
+launchctl unload -w /System/Library/LaunchAgents/com.apple.rcd.plist 2> /dev/null
 ###############################################################################
 # Activity Monitor                                                            #
 ###############################################################################

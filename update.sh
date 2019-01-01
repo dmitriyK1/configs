c858b47448a0ffc4e8e2d434a8b73f27b512c2bf
Dmitriy Kaminskiy <dimaroxx@gmail.com>
2019-01-01 23:49:52 +0200

    update update.sh

--- update.sh
+++ update.sh
@@ -28,6 +28,7 @@ pip2 install neovim --force
 pip3 install --upgrade pip
 pip3 install neovim --force
 # upgrade all global packages
+pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U
 pip3 list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip3 install -U
 
 echo 'updating homebrew'

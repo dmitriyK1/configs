412206edf60a3ba2ab5d4b37c3d55b0ec6c5fadc
Dmitriy Kaminskiy <dimitriy.kaminskiy@nextiva.com>
2019-01-02 02:09:00 +0200

    add oni config

---
+++ oni/config.tsx
@@ -0,0 +1,43 @@
+import * as React from "react"
+import * as Oni from "oni-api"
+
+export const activate = (oni: Oni.Plugin.Api) => {
+    console.log("config activated")
+
+    // Input
+    //
+    // Add input bindings here:
+    //
+    oni.input.bind("<c-enter>", () => console.log("Control+Enter was pressed"))
+    oni.input.bind("<c-cmd-g>", "vcs.sidebar.toggle")
+
+    //
+    // Or remove the default bindings here by uncommenting the below line:
+    //
+    // oni.input.unbind("<c-p>")
+
+}
+
+export const deactivate = (oni: Oni.Plugin.Api) => {
+    console.log("config deactivated")
+}
+
+export const configuration = {
+    //add custom config here, such as
+    "autoUpdate.enabled": true,
+    "configuration.editor": "typescript",
+    "editor.fullScreenOnStart": true,
+
+    "ui.colorscheme": "gruvbox",
+
+    //"oni.useDefaultConfig": true,
+    //"oni.bookmarks": ["~/Documents"],
+    //"oni.loadInitVim": false,
+    "editor.fontSize": "16px",
+    "editor.fontFamily": "Fira Code",
+
+    // UI customizations
+    "ui.animations.enabled": true,
+    "ui.fontSmoothing": "auto",
+    "experimental.vcs.sidebar": true,
+}

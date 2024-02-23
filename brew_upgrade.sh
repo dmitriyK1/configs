75fc098c99fbad797c813de79060516693d2ebc7
Dmitriy Kaminskiy <dmitriy.kaminskiy.dev@gmail.com>
2024-02-23 21:03:30 +0200

    update brew_upgrade

--- brew_upgrade.sh
+++ brew_upgrade.sh
@@ -2,9 +2,19 @@
 
 begin
     sudo --validate
+    echo ""
+    echo ">>> brew update"
     brew update
+    echo ""
+    echo ">>> brew upgate --greedy"
     brew upgrade --greedy
+    echo ""
+    echo ">>> brew upgrade --cask --greedy"
     brew upgrade --cask --greedy
+    echo ""
+    echo ">>> brew cleanup"
     brew cleanup
+    echo ""
+    echo ">>> brew tap --repair"
     brew tap --repair
 end | tee brew.log

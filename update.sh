6dae7e197b8a6a5ac6f21ee4c27c4b6c6c791992
Dmitriy Kaminskiy <dmitriy.kaminskiy.dev@gmail.com>
2024-04-07 17:44:41 +0300

    add colorization to update.sh

--- update.sh
+++ update.sh
@@ -1,20 +1,25 @@
 #!/usr/bin/env fish
 
-echo 'start updating ...'
+function colorize_text
+    echo ""
+    echo -e "\e[1;32m$argv\e[0m"
+end
+
+colorize_text '>>> start updating ...'
 sudo --validate
 
-echo 'synchronizing configs'
+colorize_text '>>> synchronizing configs'
 cd ~/configs
 legit sync
 
-echo 'purging autojump database from non-existing paths'
+colorize_text '>>> purging autojump database from non-existing paths'
 autojump --purge
 
-echo 'updating vim'
+colorize_text '>>> updating vim'
 vim +PlugUpgrade +PlugUpdate +qall!
 ~/.vim/plugged/cpsm/install.sh
 
-echo 'updating neovim'
+colorize_text '>>> updating neovim'
 nvim +PlugUpgrade +PlugUpdate +PlugClean +qall!
 
 # echo 'updating doom emacs'
@@ -27,7 +32,7 @@ if type -q doom
   doom --force upgrade
 end
 
-echo 'updating python packages'
+colorize_text '>>> updating python packages'
 pip2 install --upgrade pip
 pip2 install neovim --force
 pip3 install --upgrade pip
@@ -40,37 +45,38 @@ pip3 install neovim --force
 python2 -m pip_upgrade_outdated --verbose
 python3 -m pip_upgrade_outdated --verbose
 
-echo 'updating homebrew'
+colorize_text '>>> updating homebrew'
 ./brew_upgrade.sh
 
-echo 'updating fish shell'
+colorize_text '>>> updating fish shell'
 fisher update
 fish_update_completions
 omf update
 
-echo 'checking Apple Updates'
+colorize_text '>>> checking Apple Updates'
 /usr/sbin/softwareupdate --all --install --force
 
-echo 'updating node'
+colorize_text '>>> updating node'
 nvm install node --reinstall-packages-from=node
 
-echo 'updating npm'
+colorize_text '>>> updating npm'
 command npm install npm -g # store modules in ~/.node_modules_global via `npm config set prefix=$HOME/.node_modules_global`
 source $HOME/npm-upgrade.sh
 
-echo 'updating yarn'
+colorize_text '>>> updating yarn'
 curl --compressed -o- -L https://yarnpkg.com/install.sh | bash
 yarn global upgrade
 
 if type -q rustup
-  echo 'updating rustup'
+  colorize_text '>>> updating rustup'
   rustup update
 end
 
 if type -q port
-  echo 'updating macports'
+  colorize_text '>>> updating macports'
   sudo port selfupdate
-  sudo port upgrade outdated
+  sudo port -d sync
+  sudo port -R -N -c upgrade outdated
 end
 
 # echo 'updating hosts'
